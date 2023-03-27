9c519ec15714ea2b76826ddd335b298fc51b50c6
omer-21 <73713611+omer-21@users.noreply.github.com>
2023-03-27 12:45:08 +0300

    Added Project Files

---
+++ Temporary-Note-taking-App/Note-taking App/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_taking_App
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string command, StringBuilder retstring, int Returnlenth, IntPtr callback);
+
+        public Form1()
+        {
+            InitializeComponent();
+            mciSendString("open new Type waveaudio alias recsound", null, 0, IntPtr.Zero);
+            startR.Click += new EventHandler(this.startR_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 183;
+
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMessage.Text);
+
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = dataGridView1.CurrentCell.RowIndex;
+
+                if (index > -1)
+                {
+                    txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                    txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+                }
+            }catch(NullReferenceException ev)
+            {
+                Console.WriteLine(ev.ToString());
+            }
+        }
+
+        private void bttDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = dataGridView1.CurrentCell.RowIndex;
+                table.Rows[index].Delete();
+            }
+            catch (NullReferenceException ear)
+            {
+                Console.WriteLine(ear.ToString());
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void startR_Click(object sender, EventArgs e)
+        { 
+            //throw new NotImplenentedException();
+            mciSendString("record recsound", null, 0, IntPtr.Zero);
+            startR.Click += new EventHandler(this.endR_Click);
+        }
+
+        private void endR_Click(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            mciSendString("save recsound c:\\tryAdiuo\\try1.wav", null, 0, IntPtr.Zero);
+            mciSendString("close recsound ", null, 0, IntPtr.Zero);
+        }
+    }
+}
